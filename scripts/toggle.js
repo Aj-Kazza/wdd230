bfa6e03bf736ddc0615ddbe778f6e870b4b4acc2
Aj-Kazza <kat21008@byui.edu>
2023-06-18 18:41:15 +1200

    dark mode toggle

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,14 +1,26 @@
 const modeButton = document.querySelector("#mode");
-const body = document.querySelector("body")
+const body = document.querySelector("body");
+const main = document.querySelector("main");
+let sections = document.querySelectorAll("section");
+let h3 = document.querySelectorAll('h3');
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("🌙")) {
 		body.style.background = "#000";
-		body.style.color = "#fff";
+		main.style.color = "#FFD100";
+		for (const section of sections) {
+			section.style.background = '#000'
+		}
 		modeButton.textContent = "☀️";
+		for (const i of h3) {
+			i.style.color = 'black'
+		}
 	} else {
-		body.style.background = "#eee";
-		body.style.color = "#000";
+		body.style.background = "white";
+		main.style.color = "#000";
 		modeButton.textContent = "🌙";
+		for (const section of sections) {
+			section.style.background = '#E0E0E0'
+		}
 	}
 });

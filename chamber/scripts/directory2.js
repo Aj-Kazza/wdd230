24a5d5feac6cff311db6489dc182cb04b07bacf8
Aj-Kazza <kat21008@byui.edu>
2023-07-20 17:58:46 +1200

    almost done

--- chamber/scripts/directory2.js
+++ chamber/scripts/directory2.js
@@ -1,10 +1,50 @@
 const baseURL = "https://aj-kazza.github.io/wdd230/chamber/";
-const linksURL = "https://aj-kazza.github.io/wdd230/chamber/data/members.json";
+//const linksURL = "https://aj-kazza.github.io/wdd230/chamber/data/members.json";
 const cards = document.querySelector('article')
-
+const linksURL = "data/members.json"
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    console.log(data.members);
+    displayMembers(data.members);
+}
+
+getLinks();
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        // Create elements to add to the div.cards element
+        let card = document.createElement('section');
+        let fullName = document.createElement('h3'); // fill in the blank
+        let logo = document.createElement('img');
+        let address = document.createElement('p');
+        let phone = document.createElement('p');
+        let url = document.createElement('a');
+        let level = document.createElement('p');
+
+
+        // Build the h2 content out to show the member's full name
+        fullName.textContent = `${member.name}`; // fill in the blank
+        // Build the image logo by setting all the relevant attributes
+        logo.setAttribute('src', member.image);
+        logo.setAttribute('alt', `Logo of ${member.name}`); // fill in the blank
+        logo.setAttribute('loading', 'lazy');
+        logo.setAttribute('width', '340');
+        logo.setAttribute('height', '440');
+        address.textContent = `${member.birthdate}`;
+        phone.textContent = `${member.birthplace}`;
+        url.textContent = `${member.website}`;
+        url.setAttribute('src', member.website)
+        level.textContent = 
+
+        // Append the section(card) with the created elements
+        card.appendChild(fullName); //fill in the blank
+        card.appendChild(logo);
+        card.appendChild(address)
+        card.appendChild(phone)
+        card.appendChild(url)
+
+        cards.appendChild(card);
+    }); // end of arrow function and forEach loop
 }
